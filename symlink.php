40740fbf2a555e22847e5a5270ebbdb5794cc362
Abhishek Mathur (mathur17021play@gmail.com) <mathur17021play@gmail.com>
2019-08-15 09:21:56 +0530

    initial push

---
+++ symlink.php
@@ -0,0 +1,4 @@
+<?php
+// echo __dir__;
+symlink('/var/www/vhosts/backpocket.ca/wallet.mybackpocket.io/shoppingcart/storage/app/public', '/var/www/vhosts/backpocket.ca/wallet.mybackpocket.io/storage');
+?>
